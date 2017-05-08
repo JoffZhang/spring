c9b9ce2ce7d7375f2f77f802c953a28c333bff91
yunzhang <zhangyun@aero-edu.com>
2017-05-08 13:57:56 +0800

    config server

---
+++ config/cloud-config-server/src/main/java/com/spring/Application.java
@@ -0,0 +1,17 @@
+package com.spring;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Hello world!
+ * 添加@EnableConfigServer注解，开启Config Server
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
